a0e1e9d34b567f982fa9981c0020b0141c47b3b6
Stefan <1708541+stefankn@users.noreply.github.com>
2025-08-16 20:46:16 +0200

    Setup LLM provider based on environment variable

--- Maeve/Extensions/AiClientServiceCollectionExtensions.cs
+++ Maeve/Extensions/AiClientServiceCollectionExtensions.cs
@@ -13,34 +13,50 @@ public static class AiClientServiceCollectionExtensions {
     // - Functions
 
     public static IServiceCollection ConfigureAiClient(this IServiceCollection services, ConfigurationManager configuration) {
-        using var factory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+        var llmProvider = Environment.GetEnvironmentVariable("LLM_PROVIDER")?.AsProvider();
 
-        // Ollama
-        // var host = Environment.GetEnvironmentVariable("OLLAMA_HOST") ?? "host.docker.internal";
-        // var port = Environment.GetEnvironmentVariable("OLLAMA_PORT") ?? "11434";
-        // var client = new OllamaApiClient($"http://{host}:{port}");
-        // var chatClient = new ChatClientBuilder(client)
-        //     //.UseLogging(factory)
-        //     .UseFunctionInvocation()
-        //     .Build();
-        //
-        // services.AddSingleton<IModelProvider>(provider => {
-        //     var keyValueStore = provider.GetRequiredService<IKeyValueStore>();
-        //     return new OllamaModelProvider(client, keyValueStore);
-        // });
+        return llmProvider switch {
+            Provider.Ollama => services.SetupOllamaClient(),
+            Provider.Antrophic => services.SetupAntrophicClient(configuration),
+            _ => throw new Exception("LLM_PROVIDER environment variable is not set or unknown value")
+        };
+    }
+
+    private static IServiceCollection SetupOllamaClient(this IServiceCollection services) {
+        using var factory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
         
-        // Claude
+        var host = Environment.GetEnvironmentVariable("OLLAMA_HOST") ?? "host.docker.internal";
+        var port = Environment.GetEnvironmentVariable("OLLAMA_PORT") ?? "11434";
+        var ollamaClient = new OllamaApiClient($"http://{host}:{port}");
+        var chatClient = new ChatClientBuilder(ollamaClient)
+            //.UseLogging(factory)
+            .UseFunctionInvocation()
+            .Build();
+        
+        services.AddSingleton<IModelProvider>(provider => {
+            var keyValueStore = provider.GetRequiredService<IKeyValueStore>();
+            return new OllamaModelProvider(ollamaClient, keyValueStore);
+        });
+                
+        return services
+            .AddDistributedMemoryCache()
+            .AddSingleton(chatClient);
+    }
+
+    private static IServiceCollection SetupAntrophicClient(this IServiceCollection services, ConfigurationManager configuration) {
+        using var factory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+        
         var client = new AnthropicClient(new APIAuthentication(configuration["Antrophic:ApiKey"]));
         var chatClient = client.Messages
             .AsBuilder()
-            .UseLogging(factory)
+            //.UseLogging(factory)
             .UseFunctionInvocation()
             .Build();
         services.AddSingleton<IModelProvider>(provider => {
             var keyValueStore = provider.GetRequiredService<IKeyValueStore>();
             return new AntrophicModelProvider(client, keyValueStore);
         });
-
+                
         return services
             .AddDistributedMemoryCache()
             .AddSingleton(chatClient);
--- Maeve/Extensions/StringExtensions.cs
+++ Maeve/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using System.Text.RegularExpressions;
+using Maeve.ModelProviders;
 
 namespace Maeve.Extensions;
 
@@ -8,4 +10,17 @@ public static class StringExtensions {
     
     public static string Truncate(this string value, int maxLength) => value.Length <= maxLength ? value : value[..maxLength] + "...";
     public static string DashToCamelCase(this string value) => Regex.Replace(value, "-.", m => m.Value.ToUpper()[1..]);
+
+    public static Provider? AsProvider(this string value) {
+        foreach (var field in typeof(Provider).GetFields()) {
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is not DescriptionAttribute attribute)
+                continue;
+            
+            if (attribute.Description == value) {
+                return (Provider?)field.GetValue(null);
+            }
+        }
+
+        return null;
+    }
 }
